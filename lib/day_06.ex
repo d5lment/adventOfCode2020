46b36ac3b9582bbdd8e86b5f520adba9b9bce3ae
Paola Reyes <d5lment@gmail.com>
2021-03-12 22:37:43 -0600

    Beging with day 06

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -3,22 +3,13 @@ defmodule Day06 do
   Documentation for `Day 06`.
   """
   def get_entries() do
-    file_path = "/Users/d5lment/workspace/D5lmentsDojo/adventOfCode2020/advent_of_code_2020/lib/resources/day_04/input_06_sample.txt"
+    file_path = "/Users/d5lment/workspace/D5lmentsDojo/adventOfCode2020/advent_of_code_2020/lib/resources/day_06/input_06_sample.txt"
     {:ok, contents} = File.read(file_path)
 
     contents
-    |> String.split("\n\n", trim: true)
-    |> Enum.map(fn x -> String.replace(x, " ", "\n")  end)
-    |> Enum.map(fn x -> String.split(x, "\n", trim: true)  end)
-    |> Enum.map(fn x ->
-        Enum.map(x, fn fields ->
-          String.split(fields, ":", trim: true)
-        end)
-      end)
-    |> Enum.map(fn pairs ->
-        Enum.reduce(pairs, [], fn ([key, val], acc) ->
-        [{String.to_atom(key), val} | acc]
-      end)
-    end)
+    |> Enum.map(fn x ->  String.split(x, "\n", trim: true) end)
+    # |> Enum.map(fn x -> String.graphemes(x)  end)
+    # |> Enum.map(fn x -> String.split(x, "\n", trim: true)  end)
+    # |> String.split("\n\n", trim: true)
   end
 end
--- test/day_06_test.exs
+++ test/day_06_test.exs
@@ -2,4 +2,6 @@ defmodule Day06Test do
   use ExUnit.Case
   doctest Day06
 
+  # {:ok, "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"}
+
 end
